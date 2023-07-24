f49f9a2b398068492d2e9241eba0249df8d9cc78
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-07-24 08:54:45 +0000

    estrellas a la velocidad de la ISS

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 function setup() {
-    loadJSON("https://api.wheretheiss.at/v1/satellites/25544", gotData);
-  }
+  createCanvas(1000, 1000);
+  loadJSON("https://api.wheretheiss.at/v1/satellites/25544", gotData);
+}
 
-  function gotData(data){
-    print(data);
+function gotData(data){
+  background(0);
+  for (let i = 0; i < data.velocity/100; i++) {
+    fill(255);
+    ellipse(random(width),random(height), 5, 5);
+    
   }
-  
-  function draw() {
-    background(220);
-  }
+}
+
