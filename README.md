9de31be624f149395a19777e1476f0d989c63554
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-07-27 01:53:08 +0000

    primer commit

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A little excersice using JSON URL
 API page:
 
 https://home.openweathermap.org/api_keys
+
+
+
+
+separador:
+https://www.youtube.com/watch?v=ecT42O6I_WI&list=PLRqwX-V7Uu6a-SQiI4RtIwuOrLJGnel0r&index=5&t=40s  -- 13:08
--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
+var weather;
+
 function setup() {
   createCanvas(1000, 1000);
-  loadJSON("https://api.wheretheiss.at/v1/satellites/25544", gotData);
+  loadJSON("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=c6cd1da1cbda63c234da764f27d3de93", gotData);
 }
 
 function gotData(data){
-  background(0);
-  for (let i = 0; i < data.velocity/100; i++) {
-    fill(255);
-    ellipse(random(width),random(height), 5, 5);
-    
+print(data)
+ // weather = data;
+  }
+
+function draw(){
+  if (weather){
+
   }
 }
 
