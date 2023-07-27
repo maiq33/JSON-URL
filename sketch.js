4809402a47a3cf653e7b55998a85d783fe4b28c2
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-07-27 01:58:38 +0000

    segundo commit

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 var weather;
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(400, 400);
   loadJSON("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=c6cd1da1cbda63c234da764f27d3de93", gotData);
 }
 
 function gotData(data){
-print(data)
- // weather = data;
+  //print(data)
+  weather = data;
   }
 
 function draw(){
+  background(0);
   if (weather){
+    ellipse()
 
   }
 }
