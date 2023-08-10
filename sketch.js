e03198da0d03a41daaa76b36f130c1e51824e196
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-10 03:00:02 +0000

    inicio leccion 10.6

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var weather;
+var url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=c6cd1da1cbda63c234da764f27d3de93&units=metric";
 
 function setup() {
   createCanvas(400, 400);
-  loadJSON("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=c6cd1da1cbda63c234da764f27d3de93&units=metric", gotData);
+  loadJSON( url, gotData);
 }
 
 function gotData(data){
@@ -13,8 +14,12 @@ function gotData(data){
 function draw(){
   background(0);
   if (weather){
-    ellipse(50, 100, weather.main.temp, weather.main.temp);
-    ellipse(200, 100, weather.main.humidity, weather.main.humidity);
+
+    var temp = weather.main.temp;
+    var humidity = weather.main.humidity
+
+    ellipse(50, 100, temp, temp);
+    ellipse(200, 100, humidity, humidity);
 
   }
 }
