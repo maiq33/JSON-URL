43d7ab9a6a63e6c7ed488a4e6e3155cc013f96e2
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-10 02:21:02 +0000

    final de la leccion 10.5

--- sketch.js
+++ sketch.js
@@ -2,18 +2,19 @@ var weather;
 
 function setup() {
   createCanvas(400, 400);
-  loadJSON("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=c6cd1da1cbda63c234da764f27d3de93", gotData);
+  loadJSON("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=c6cd1da1cbda63c234da764f27d3de93&units=metric", gotData);
 }
 
 function gotData(data){
-  //print(data)
+  print(data)
   weather = data;
   }
 
 function draw(){
   background(0);
   if (weather){
-    ellipse()
+    ellipse(50, 100, weather.main.temp, weather.main.temp);
+    ellipse(200, 100, weather.main.humidity, weather.main.humidity);
 
   }
 }
