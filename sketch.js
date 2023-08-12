d5ba2d170c539ddd865b28b52205d7b039fa28f6
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-12 00:21:48 +0000

    la pagina fue exitosa, ChatGPT mejoro el codigo

--- sketch.js
+++ sketch.js
@@ -1,79 +1,50 @@
-// Variables para guardar información del clima
-var weather;
+const weatherAPI = {
+  baseURL: "https://api.openweathermap.org/data/2.5/weather?",
+  geoURL: "https://api.openweathermap.org/geo/1.0/direct?",
+  key: "c6cd1da1cbda63c234da764f27d3de93",
+  units: "metric"
+};
 
-// URLs base y parámetros para hacer las solicitudes a la API de OpenWeatherMap
-var api = "https://api.openweathermap.org/data/2.5/weather?";
-var lat = "lat=44.34";
-var lon = "&lon=10.99";
-var key = "&appid=c6cd1da1cbda63c234da764f27d3de93";
-var units = "&units=metric";
+let weather;
 
-
-
-// La función setup se ejecuta una vez al inicio
 function setup() {
-  // Crea un canvas de 400x400 pixeles
   createCanvas(400, 400);
-
-  // Selecciona el botón con el ID "submit" y asigna el evento mousePressed a la función selectCity
-  var button = select("#submit");
+  const button = select("#submit");
   button.mousePressed(selectCity);
-  
 }
 
-// Función para solicitar información del clima basada en latitud y longitud
-function weatherAsk(lat, lon){
-  // Construye la URL completa con los parámetros de latitud, longitud, clave API y unidades
-  var url = api + "lat=" + lat + "&lon=" + lon + "&appid=c6cd1da1cbda63c234da764f27d3de93" + units;
-  // Solicita los datos en formato JSON y llama a la función gotData cuando se reciban
+const weatherAsk = (lat, lon) => {
+  const url = `${weatherAPI.baseURL}lat=${lat}&lon=${lon}&appid=${weatherAPI.key}&units=${weatherAPI.units}`;
   loadJSON(url, gotData);
 }
 
-// Función llamada cuando se presiona el botón
-function selectCity(){
+const selectCity = () => {
+  const input = select("#cityInput");
+  const city = input.value();
+  const urlCity = `${weatherAPI.geoURL}q=${city}&limit=1&appid=${weatherAPI.key}`;
 
-  var apiCity = "https://api.openweathermap.org/geo/1.0/direct?";
-  // Selecciona el valor ingresado en el input con ID "cityInput"
-  var input = select("#cityInput"); 
-  var city = input.value();
-  var limit = "&limit=1";
-  var keyCity = "&appid=c6cd1da1cbda63c234da764f27d3de93";
-
-  // Construye la URL para obtener la latitud y longitud de la ciudad ingresada
-  var urlCity = apiCity+"q="+city+limit+keyCity;
-
-  // Solicita los datos de la ciudad
-  loadJSON(urlCity, function(cityData) {
-    // Si recibe datos de la ciudad, obtiene la latitud y longitud y luego llama a weatherAsk
+  loadJSON(urlCity, cityData => {
     if (cityData && cityData.length > 0) {
-      var latCity = cityData[0].lat;
-      var lonCity = cityData[0].lon;
+      const latCity = cityData[0].lat;
+      const lonCity = cityData[0].lon;
       weatherAsk(latCity, lonCity);
     }
   });
 }
 
+const gotData = (data) => {
+  print(data);
+  weather = data;
+}
 
-// Función que se ejecuta una vez que se reciben los datos del clima
-function gotData(data){
-  print(data) // Imprime los datos en la consola
-  weather = data; // Asigna los datos a la variable weather
-  }
-
-function draw(){
-  background(0); // Pone el fondo del canvas de color negro
-
-  // Si hay datos en la variable weather
-  if (weather){
-
-     // Obtiene la temperatura y la humedad de los datos
-    var temp = weather.main.temp;
-    var humidity = weather.main.humidity;
-
-    // Dibuja dos elipses en el canvas basándose en la temperatura y humedad
-    ellipse(50, 100, temp, temp); // El tamaño de la elipse es proporcional a la temperatura
-    ellipse(200, 100, humidity, humidity);  // El tamaño de la elipse es proporcional a la humedad
-
+function draw() {
+  background(0);
+  if (weather) {
+    const temp = weather.main.temp;
+    const humidity = weather.main.humidity;
+    ellipse(50, 100, temp, temp);
+    ellipse(200, 100, humidity, humidity);
   }
 }
+
 
