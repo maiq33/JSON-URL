08fa88771005e3ec5f1837fddecdc122d535dc5e
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-15 02:37:47 +0000

    1st commit

--- sketch.js
+++ sketch.js
@@ -1,14 +1,9 @@
 function setup() {
-  createCanvas(1000, 1000);
-  loadJSON("https://api.wheretheiss.at/v1/satellites/25544", gotData);
+
 }
 
-function gotData(data){
+function draw(){
   background(0);
-  for (let i = 0; i < data.velocity/100; i++) {
-    fill(255);
-    ellipse(random(width),random(height), 5, 5);
-    
-  }
+  
 }
 
